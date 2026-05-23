1dc4ec3426504ff1544ea582aa2aac4ee146f769
Karim Essam <KaEssam17@gmail.com>
2026-05-23 16:26:39 +0300

    (lesson): 08-built-in-pipe

---
+++ src/app/pipe/pipe.ts
@@ -0,0 +1,46 @@
+import {
+  AsyncPipe,
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+  JsonPipe,
+  PercentPipe,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-pipe',
+  imports: [
+    DatePipe,
+    CurrencyPipe,
+    DecimalPipe,
+    PercentPipe,
+    UpperCasePipe,
+    TitleCasePipe,
+    SlicePipe,
+    JsonPipe,
+    AsyncPipe,
+  ],
+  templateUrl: './pipe.html',
+  styleUrl: './pipe.css',
+})
+export class Pipe {
+  today = new Date();
+  price = 1234.5678;
+  pi = 3.1415926;
+  ratio = 0.4784;
+
+  name = 'karim essam';
+  bio = 'ddaskdjalksdjas ddjaksdh lasd dd;aklshd a ;jd a;sd ja';
+
+  user = {
+    name: 'karim',
+    email: 'karim@mail.com',
+  };
+
+  test = interval(1000);
+}
