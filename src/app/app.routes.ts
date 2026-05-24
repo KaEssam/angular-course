be0d1505a0ea31b1db5f5429b95cfb1b7298811d
Karim Essam <KaEssam17@gmail.com>
2026-05-24 16:49:00 +0300

    (lesson): 013-routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { UserCard } from './user-card/user-card';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: App },
+  // { path: 'user', component: User },
+  { path: 'user', loadComponent: () => import('./user/user').then((m) => m.User) },
+  { path: 'user-card', component: UserCard },
+];
