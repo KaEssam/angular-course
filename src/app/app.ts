bf99a5ade0cddf760ae5221abd237edba060efb8
Karim Essam <KaEssam17@gmail.com>
2026-05-24 16:35:15 +0300

    (lesson): 012-interceptors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular');
+
+  http = inject(HttpClient);
+  token = signal('');
+
+  getAll() {
+    this.http.get('https://jsonplaceholder.typicode.com/todos').subscribe(console.log);
+  }
+
+  login() {
+    console.log('login');
+    this.http
+      .post<any>('http://linkvaultapi.runasp.net/api/auth/login', {
+        email: 'angular@mail.com',
+        password: 'Angular@123!',
+      })
+      .subscribe((data) => {
+        localStorage.setItem('token', data.token);
+        console.log(data);
+      });
+  }
+
+  cat() {
+    this.http.get<any>('http://linkvaultapi.runasp.net/api/categories').subscribe(console.log);
+  }
 }
