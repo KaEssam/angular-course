c033f289a7e8fcd44c4d9ec156805ad00a931a3f
Karim Essam <KaEssam17@gmail.com>
2026-05-25 15:12:16 +0300

    (lesson): 015-template-forms

---
+++ src/app/login/login.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  email = '';
+  password = '';
+
+  onSumbit(form: NgForm) {
+    if (form.invalid) return;
+    console.log(form.value);
+  }
+}
